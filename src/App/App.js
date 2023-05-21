c3c820054dc2e164e08e9e0f4b0ae216bdf71061
Ahammed Saad <a.saad227@yahoo.com>
2023-05-21 21:19:51 +0100

    fetch function created and display selected location weather details

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Fetch from '../Fetch';
+
+function App() {
+  return (
+    <div className="App">
+      <Fetch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Fetch/index.js
@@ -0,0 +1,43 @@
+import React,{useEffect, useState} from 'react'
+
+export default function Fetch() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+            fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=London&aqi=no`)
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+    
+
+  return (
+    <div>
+        <h1>My Weather App</h1>
+        {loading && <p>Loading...</p>}
+        {error && <p>Something went wrong: {error.message}</p>}
+        {data && (  
+            <div>
+                <h1>{data.location.name}</h1>
+                <p>{data.location.region}</p>
+                <p>{data.location.country}</p>
+                <p>{data.location.localtime}</p>
+                <p>{data.current.temp_c}</p>
+                <p>{data.current.condition.text}</p>
+                <img src={data.current.condition.icon} alt={data.current.condition.text} />
+                </div>)}
+    </div>
+  )
+}
+
+
