395a683f7c833cdcc51fc050a1f14f6370ab78bc
Ahammed Saad <a.saad227@yahoo.com>
2023-05-21 23:54:28 +0100

    update component and also update css

--- src/App/App.js
+++ src/App/App.js
@@ -1,42 +1,62 @@
-import React,{useState} from 'react';
-
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Fetch from '../Fetch';
 import WeatherLocation from '../WeatherLocation';
 
 function App() {
   const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [location, setLocation] = useState("");
-  
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [location, setLocation] = useState("");
+
   async function fetchData() {
     setLoading(true);
     setError(null);
     setData(null);
     try {
+      if (location === "") {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=London&aqi=no`);
+        if (response.ok) {
+          const json = await response.json();
+          setData(json);
+        
+        } else {
+          throw response;
+          
+        }
+      } else {
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&aqi=no`);
         if (response.ok) {
-            const json = await response.json();
-            setData(json);
+          const json = await response.json();
+          setData(json);
+         
         } else {
-            throw response;
+          throw response;
         }
+      }
+
+
     } catch (error) {
-        console.error("Error fetching data: ", error);
-        setError(error);
+      console.error("Error fetching data: ", error);
+      setError(error);
     }
     setLoading(false);
-}
+    setLocation("")
+  }
+
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
-      <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+      <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder='Enter your location here....' />
       <button type="button" onClick={fetchData}>Search</button>
-      
-    <div className='item'>
-    <Fetch/>
-    <WeatherLocation data={data} loading={loading} error={error}/>  
-    </div>
+  
+        <WeatherLocation data={data} loading={loading} error={error} />
+     
     </div>
   );
 }
