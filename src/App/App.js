4a9e90712c572404526df5d48ecf42efda808c58
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-05-22 23:20:47 +0100

    7 day forecast added and also some css update

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import WeatherLocation from '../WeatherLocation';
+import WeekForecast from '../WeekCast/weekforecast';
 
 function App() {
   const [data, setData] = useState(null);
@@ -43,21 +44,61 @@ function App() {
     setLoading(false);
     setLocation("")
   }
+  
+  async function fetchWeekData() {
+    setLoading(true);
+    setError(null);
+    setData(null);
+    try {
+      if (location === "") {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=London&days=7`);
+        if (response.ok) {
+          const json = await response.json();
+          setData(json);
+
+        } else {
+          throw response;
+
+        }
+      } else {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&days=7`);
+        if (response.ok) {
+          const json = await response.json();
+          setData(json);
+
+        } else {
+          throw response;
+        }
+      }
 
 
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setError(error);
+    }
+    setLoading(false);
+    setLocation("")
+  }
+  
+  const forecastday = data?.forecast?.forecastday || [];
+
+function handleFunctin(){
+  fetchData();
+  fetchWeekData();
+}
 
   useEffect(() => {
-    fetchData();
+   handleFunctin();
   }, []);
 
   return (
     <div className='App'>
       <h1 className='header'>Weather App</h1>
       <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder='Enter your location here....' />
-      <button type="button" onClick={fetchData}>Search</button>
+      <button type="button" onClick={handleFunctin}>Search</button>
   
         <WeatherLocation data={data} loading={loading} error={error} />
-     
+     <WeekForecast forecastday={forecastday} loading={loading} error={error} />
     </div>
   );
 }
---
+++ src/WeekCast/weekforecast.js
@@ -0,0 +1,24 @@
+import React,{useState, useEffect} from 'react'
+
+export default function WeekForecast({forecastday,loading,error}) {
+
+
+
+  return (
+    <div className='flex-forecast'>
+{loading && <p>Loading...</p>}
+{error && <p>Something went wrong: {error.message}</p>}
+{forecastday.map((day) => (
+    <div className='forecast-container'>
+        <p>Date: {day.date}</p>
+        <p>Sunrise: {day.astro.sunrise}</p>
+        <p>Sunset: {day.astro.sunset}</p>
+        <p>Maxtemp: {day.day.maxtemp_c}</p>
+        <p>Mintemp: {day.day.mintemp_c}</p>
+        <p>{day.day.condition.text}</p>
+        <img src={day.day.condition.icon} alt={day.day.condition.text} />
+    </div>
+  ))}
+    </div>
+    )
+}
